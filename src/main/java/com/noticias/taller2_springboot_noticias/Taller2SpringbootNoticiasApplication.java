b6fa4e00288ba8f9802f1841bcfbc313a0df78b7
tobdevelo <jtajana@unicartagena.edu.co>
2026-09-17 21:43:13 -0500

    feat: inicializacion de proyecto Spring Boot con Maven y dependencias

---
+++ src/main/java/com/noticias/taller2_springboot_noticias/Taller2SpringbootNoticiasApplication.java
@@ -0,0 +1,13 @@
+package com.noticias.taller2_springboot_noticias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller2SpringbootNoticiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller2SpringbootNoticiasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noticias/taller2_springboot_noticias/Taller2SpringbootNoticiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noticias.taller2_springboot_noticias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller2SpringbootNoticiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
