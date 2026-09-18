437764b61990e4c317278a4d09f343e7d3d0e3f7
tobdevelo <jtajana@unicartagena.edu.co>
2026-09-18 00:17:17 -0500

    fix: reestructurar paquetes bajo com.noticias para habilitar controladores

--- src/main/java/com/noticias/Taller2SpringbootNoticiasApplication.java
+++ src/main/java/com/noticias/Taller2SpringbootNoticiasApplication.java
@@ -1,4 +1,4 @@
-package com.noticias.taller2_springboot_noticias;
+package com.noticias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
